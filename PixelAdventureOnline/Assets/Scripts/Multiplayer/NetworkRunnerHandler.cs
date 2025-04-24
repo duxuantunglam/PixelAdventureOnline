e095cb9fbbea935c47d5d5835889417b7c592479
duxuantunglam <duxuantunglam2k2@gmail.com>
2025-04-25 03:04:24 +0700

    networkrunnerhandler

---
+++ PixelAdventureOnline/Assets/Scripts/Multiplayer/NetworkRunnerHandler.cs
@@ -0,0 +1,58 @@
+using Fusion;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NetworkRunnerHandler : MonoBehaviour
+{
+    public NetworkRunner runner { get; private set; }
+    [SerializeField] NetworkRunner NetworkRunner;
+
+    // [Header("Scene & GameMode")]
+    // [SerializeField] private GameMode gameMode = GameMode.Host;
+
+    private async void StartGame(GameMode gameMode)
+    {
+        runner = gameObject.AddComponent<NetworkRunner>();
+        runner.ProvideInput = true;
+
+        var scene = SceneRef.FromIndex(SceneManager.GetActiveScene().buildIndex);
+        var sceneInfo = new NetworkSceneInfo();
+        if (scene.IsValid)
+        {
+            sceneInfo.AddSceneRef(scene, LoadSceneMode.Additive);
+        }
+
+        try
+        {
+            await runner.StartGame(new StartGameArgs
+            {
+                GameMode = gameMode,
+                SessionName = "TestRoom",
+                Scene = scene,
+                SceneManager = gameObject.AddComponent<NetworkSceneManagerDefault>()
+            });
+
+            Debug.Log("[Fusion] runner Started: " + gameMode);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("[Fusion] Error Starting runner: " + e);
+        }
+    }
+
+    private void OnGUI()
+    {
+        if (runner == null)
+        {
+            if (GUI.Button(new Rect(0, 0, 200, 40), "Host"))
+            {
+                StartGame(GameMode.Host);
+            }
+            if (GUI.Button(new Rect(0, 40, 200, 40), "Join"))
+            {
+                StartGame(GameMode.Client);
+            }
+        }
+    }
+}
--- PixelAdventureOnline/Assets/Scripts/SinglePlayer/Player/SP_Player.cs
+++ PixelAdventureOnline/Assets/Scripts/SinglePlayer/Player/SP_Player.cs
@@ -14,7 +14,7 @@ public class SP_Player : MonoBehaviour
     private bool canBeController = false;
 
     [Header("Movement")]
-    [SerializeField] public float moveSpeed;
+    [SerializeField] private float moveSpeed;
     [SerializeField] private float jumpForce;
     [SerializeField] private float doubleJumpForce;
     private float defaultGravityScale;

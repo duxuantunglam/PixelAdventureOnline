6413c4b5f818ef9dec03c48be271132bc764c928
duxuantunglam <duxuantunglam2k2@gmail.com>
2025-04-28 15:28:56 +0700

    mp_gamemanager

--- PixelAdventureOnline/Assets/Scripts/Multiplayer/MP_FinishPoint.cs
+++ PixelAdventureOnline/Assets/Scripts/Multiplayer/MP_FinishPoint.cs
@@ -3,26 +3,13 @@ using Fusion;
 using System.Collections.Generic;
 using System.Linq;
 
-public class MP_FinishPoint : NetworkBehaviour, IPlayerLeft
+public class MP_FinishPoint : NetworkBehaviour
 {
     private Animator anim;
-
-    [Networked]
-    private NetworkDictionary<PlayerRef, NetworkBool> PlayersFinished => default;
-
-    private int totalPlayers;
-    private bool hasProcessedResults;
 
     public override void Spawned()
     {
         anim = GetComponent<Animator>();
-        hasProcessedResults = false;
-
-        if (Object.HasStateAuthority)
-        {
-            totalPlayers = Runner.ActivePlayers.Count();
-            Debug.Log($"[Fusion] Total players in room: {totalPlayers}");
-        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -31,40 +18,12 @@ public class MP_FinishPoint : NetworkBehaviour, IPlayerLeft
 
         MP_Player player = collision.GetComponent<MP_Player>();
 
-        if (player != null)
+        if (player != null && player.Object.HasInputAuthority)
         {
-            RPC_PlayerFinished(player.Object.InputAuthority);
-        }
-    }
-
-    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
-    private void RPC_PlayerFinished(PlayerRef player)
-    {
-        if (!Object.HasStateAuthority) return;
-
-        if (!PlayersFinished.ContainsKey(player))
-        {
-            PlayersFinished.Add(player, true);
-
-            Debug.Log($"[Fusion] Player {player.PlayerId} finished. Total finished: {PlayersFinished.Count}/{totalPlayers}");
+            float finishTime = (float)Runner.SimulationTime;
+            MP_GameManager.instance.RPC_SaveFinishTime(player.Object.InputAuthority, finishTime);
 
             RPC_TriggerFinishAnimation();
-
-            CheckAllPlayersFinished();
-        }
-    }
-
-    private void CheckAllPlayersFinished()
-    {
-        if (!Object.HasStateAuthority || hasProcessedResults) return;
-
-        if (PlayersFinished.Count >= totalPlayers)
-        {
-            hasProcessedResults = true;
-
-            Debug.Log("[Fusion] All players have finished!");
-
-            RPC_GameFinished();
         }
     }
 
@@ -75,34 +34,5 @@ public class MP_FinishPoint : NetworkBehaviour, IPlayerLeft
         {
             anim.SetTrigger("active");
         }
-    }
-
-    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
-    private void RPC_GameFinished()
-    {
-        if (Object.HasInputAuthority)
-        {
-            float finishTime = (float)Runner.SimulationTime;
-            SaveResults(finishTime);
-        }
-    }
-
-    private void SaveResults(float finishTime)
-    {
-        Debug.Log($"[Fusion] Saving results for local player. Finish Time: {finishTime}");
-        // TODO: Gửi dữ liệu finishTime lên Firebase hoặc lưu cục bộ
-    }
-
-    public void PlayerLeft(PlayerRef player)
-    {
-        if (!Object.HasStateAuthority) return;
-
-        if (PlayersFinished.ContainsKey(player))
-        {
-            PlayersFinished.Remove(player);
-        }
-
-        totalPlayers = Runner.ActivePlayers.Count();
-        CheckAllPlayersFinished();
     }
 }
---
+++ PixelAdventureOnline/Assets/Scripts/Multiplayer/MP_GameManager.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Fusion;
+using System.Linq;
+
+public class MP_GameManager : NetworkBehaviour, IPlayerLeft
+{
+    public static MP_GameManager instance { get; private set; }
+
+    public static event Action<PlayerRef, float> OnPlayerFinished;
+    public static event Action OnAllPlayersFinished;
+    public static event Action OnGameReset;
+
+    [Networked]
+    private NetworkDictionary<PlayerRef, float> finishTimes => default;
+
+    private int totalPlayers;
+    private bool hasGameFinished = false;
+
+    public override void Spawned()
+    {
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        if (Object.HasStateAuthority && Runner != null)
+        {
+            totalPlayers = Runner.ActivePlayers.Count();
+            Debug.Log($"[Fusion][GameManager] Total players at start: {totalPlayers}");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (instance == this)
+            instance = null;
+    }
+
+    [Rpc(RpcSources.InputAuthority, RpcTargets.StateAuthority)]
+    public void RPC_SaveFinishTime(PlayerRef player, float time)
+    {
+        if (!Object.HasStateAuthority) return;
+        if (time < 0) return;
+
+        if (!finishTimes.ContainsKey(player))
+        {
+            finishTimes.Add(player, time);
+            Debug.Log($"[Fusion][GameManager] Player {player.PlayerId} finished with time {time}");
+
+            RPC_NotifyPlayerFinished(player, time);
+
+            CheckAllPlayersFinished();
+        }
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void RPC_NotifyPlayerFinished(PlayerRef player, float time)
+    {
+        OnPlayerFinished?.Invoke(player, time);
+    }
+
+    private void CheckAllPlayersFinished()
+    {
+        if (!Object.HasStateAuthority || hasGameFinished) return;
+
+        if (finishTimes.Count >= totalPlayers)
+        {
+            hasGameFinished = true;
+            Debug.Log("[Fusion][GameManager] All players have finished!");
+
+            ProcessGameFinished();
+        }
+    }
+
+    private void ProcessGameFinished()
+    {
+        if (!Object.HasStateAuthority) return;
+
+        RPC_NotifyGameFinished();
+
+        var sortedResults = GetSortedResults();
+        foreach (var entry in sortedResults)
+        {
+            Debug.Log($"Player {entry.Key.PlayerId} => Finish Time: {entry.Value}");
+        }
+
+        // TODO: Đẩy dữ liệu lên Firebase/Database
+        // TODO: Chuyển scene sau một khoảng thời gian
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void RPC_NotifyGameFinished()
+    {
+        OnAllPlayersFinished?.Invoke();
+    }
+
+    public void PlayerLeft(PlayerRef player)
+    {
+        if (!Object.HasStateAuthority) return;
+
+        if (finishTimes.ContainsKey(player))
+        {
+            finishTimes.Remove(player);
+        }
+
+        totalPlayers = Runner.ActivePlayers.Count();
+
+        if (totalPlayers == 0)
+        {
+            ResetGame();
+        }
+        else
+        {
+            CheckAllPlayersFinished();
+        }
+    }
+
+    public float GetFinishTime(PlayerRef player)
+    {
+        if (finishTimes.TryGet(player, out float time))
+        {
+            return time;
+        }
+        return -1f;
+    }
+
+    public List<KeyValuePair<PlayerRef, float>> GetSortedResults()
+    {
+        if (finishTimes.Count == 0)
+            return new List<KeyValuePair<PlayerRef, float>>();
+
+        return finishTimes.OrderBy(pair => pair.Value).ToList();
+    }
+
+    public void ResetGame()
+    {
+        if (!Object.HasStateAuthority) return;
+
+        finishTimes.Clear();
+        hasGameFinished = false;
+        totalPlayers = Runner.ActivePlayers.Count();
+
+        RPC_NotifyGameReset();
+    }
+
+    [Rpc(RpcSources.StateAuthority, RpcTargets.All)]
+    private void RPC_NotifyGameReset()
+    {
+        OnGameReset?.Invoke();
+    }
+}

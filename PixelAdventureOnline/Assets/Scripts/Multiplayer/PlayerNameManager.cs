f3c49d15194f8d0a6c655f8e194615fca031c53b
duxuantunglam <duxuantunglam2k2@gmail.com>
2025-05-15 09:52:40 +0700

    playerNameManager update

--- PixelAdventureOnline/Assets/Scripts/Multiplayer/PlayerNameManager.cs
+++ PixelAdventureOnline/Assets/Scripts/Multiplayer/PlayerNameManager.cs
@@ -1,12 +1,21 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerNameManager : MonoBehaviour
 {
+    [SerializeField] private GameObject playerNamePanel;
     [SerializeField] private TMP_InputField playerNameInputField;
+    [SerializeField] private Button submitButton;
 
     public void SubmitName()
     {
-        FusionManager.instance._playerName = playerNameInputField.text;
+        FusionManager.instance.ConnectToLobby(playerNameInputField.text);
+        playerNamePanel.SetActive(false);
+    }
+
+    public void ActivateButton()
+    {
+        submitButton.interactable = true;
     }
 }
